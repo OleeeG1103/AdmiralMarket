980de2da8e75a84fc61a1aa20bf763675ac80fa6
Oleg <bukreevoleg3001@gmail.com>
2020-05-22 01:18:10 +0300

    bugs fixed validator

--- src/js/closePhoneNavigation.js
+++ src/js/closePhoneNavigation.js
@@ -1,8 +1,8 @@
 function closePhoneNavigation() {
 
     document.getElementById('phoneNavWrapper').style.visibility = 'hidden';
-    document.getElementById('phoneNavWrapper').style.transition = '0.6s visibility';
-    document.getElementById('phoneNavModal').style.transition = '.2s right';
+    document.getElementById('phoneNavWrapper').style.transition = '.7s visibility';
+    document.getElementById('phoneNavModal').style.transition = '.5s right';
     document.getElementById('phoneNavModal').style.right = '-100%';
 
 };
